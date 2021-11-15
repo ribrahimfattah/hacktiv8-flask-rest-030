4714d3eb999e6b5bf535d52165f1ec1d88608e00
Rasyid Ibrahim Fattah <r.ibrahimfattah@gmail.com>
2021-11-15 10:32:46 +0700

    Add Heroku deployment files

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the Connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jqdkvllwlkbwyo:726914d64686845a69d73e5c05ed3b7d42fefcb8572b7a65b58abef044b61dbe@ec2-52-204-72-14.compute-1.amazonaws.com:5432/d9rlot14lce0dt'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
